5ddf94f5176a442ea6dd3a1ef79bf1b9fde50c3c
Scott Saunders <scott.saunders@Saunders-S.local>
2022-03-14 03:29:01 -0400

    First Commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+Plugin Name: Wide Editor
+Plugin URI: https://scottsaunders.design
+Description: Makes the default WordPress editor wider.
+Version: 1.0
+Author: Scott Saunders
+Author URI: https://scottsaunders.design
+License: GPLv2 or later
+Text Domain: scottsaunders.design
+*/
+
+
+function wide_editor_js() {
+		wp_enqueue_style( 'wide_editor', plugins_url( 'wide_admin.css', __FILE__ ) );
+	}
+	add_action('admin_footer', 'wide_editor_js');
